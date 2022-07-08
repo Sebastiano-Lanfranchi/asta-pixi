a0d26ad537dd0084cbc25f09101f16ca54a9545f
Sebastiano Lanfranchi <sebastiano.lanfranchi@ff3d.com>
2022-07-08 17:34:33 +0200

    Fix: grafica + login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, ViewChild } from '@angular/core';
 import { HeaderComponent } from './components/header-components/header.component';
 import { SwitchPageService } from './services/switch-page.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-root',
@@ -21,8 +20,5 @@ export class AppComponent {
 
    login() {
     this.auth.signInWithEmailAndPassword(this.email, this.password);
-  }
-  logout() {
-    this.auth.signOut();
   }
 }
--- src/app/components/header-components/header.component.ts
+++ src/app/components/header-components/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { SwitchPageService } from '../../services/switch-page.service'
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { SwitchPageService } from '../../services/switch-page.service';
+
 
 @Component({
   selector: 'app-header',
@@ -11,12 +13,22 @@ export class HeaderComponent implements OnInit {
   //https://app.sportdataapi.com/documentation
   //https://medium.com/@charlyyy/how-to-bypass-cors-and-scrap-any-websites-using-javascript-serverless-76115eeecddd
   urlTeamsIta = "https://v3.football.api-sports.io/";
+  email: string = '';
+  password: string = '';
  
-  constructor(public switchPage: SwitchPageService) {
+  constructor(public switchPage: SwitchPageService, public auth: AngularFireAuth) {
 
   }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.auth.signOut();
+  }
+
+  login() {
+    this.auth.signInWithEmailAndPassword(this.email, this.password);
   }
 
 
